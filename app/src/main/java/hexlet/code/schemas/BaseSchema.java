33898a594e4f41eec823cda605b8b1f567ba21c2
Sergey Ivanov <beckron@mail.ru>
2024-01-07 00:18:03 +0300

    fix javadoc

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -8,14 +8,29 @@ import java.util.function.Predicate;
 public abstract class BaseSchema {
     protected Map<String, Predicate<Object>> rules = new HashMap<>();
 
+    /**
+     * Add rules to map of rules.
+     * @param ruleName
+     * @param rule
+     */
     protected void addRule(String ruleName, Predicate<Object> rule) {
         rules.put(ruleName, rule);
     }
 
+    /**
+     * Makes rule to check if object is null or empty.
+     * @return BaseSchema
+     */
     protected BaseSchema required() {
         addRule("required", t -> !(Objects.isNull(t) || t.toString().isEmpty()));
         return this;
     }
+
+    /**
+     * Checks if all rules are true.
+     * @param object
+     * @return boolean
+     */
     protected boolean isValid(Object object) {
         return rules.values().stream().allMatch(x -> x.test(object));
     }
--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-public class MapSchema extends BaseSchema {
+public final class MapSchema extends BaseSchema {
 
     private Map<String, BaseSchema> schemas;
 
